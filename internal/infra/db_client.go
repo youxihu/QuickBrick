35bac2cecace1debf5b495fd2b989e7384441e0c
youxihu <y994189@163.com>
2025-07-31 14:17:06 +0800

    [fix]修复未连接数据库阻塞的问题

--- internal/infra/db_client.go
+++ internal/infra/db_client.go
@@ -3,6 +3,7 @@ package infra
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"QuickBrick/internal/domain/ent"
 
@@ -14,10 +15,16 @@ func NewEntClient(dsn string) (*ent.Client, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed opening connection to mysql: %w", err)
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel() // 防止 context 泄漏
 
-	if err := client.Schema.Create(context.Background()); err != nil {
+	fmt.Println("正在同步数据库 schema...") // 调试输出
+
+	if err := client.Schema.Create(ctx); err != nil {
 		return nil, fmt.Errorf("failed creating schema resources: %w", err)
 	}
 
+	fmt.Println("数据库 schema 同步完成") // 成功提示
 	return client, nil
+
 }
